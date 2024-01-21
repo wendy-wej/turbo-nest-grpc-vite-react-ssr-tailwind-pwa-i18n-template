04f5a72d2397770fb20e9dc8cb610fa9fee06665
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2024-01-21 21:37:56 +0100

    assignment

--- hms-apps/api-gateway/src/app.module.ts
+++ hms-apps/api-gateway/src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,10 +8,14 @@ import { ClinicsModule } from './clinics/clinics.module';
 import { PharmaciesModule } from './pharmacies/pharmacies.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join} from 'path';
 
 
 @Module({
-  imports: [EmployeesModule, PatientsModule, ClinicsModule, PharmaciesModule, AuthModule, UsersModule],
+  imports: [ServeStaticModule.forRoot({rootPath: join(__dirname, '../../..', 'users-demo-frontend','dist'),}),
+            EmployeesModule, PatientsModule, ClinicsModule, PharmaciesModule, AuthModule, UsersModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
